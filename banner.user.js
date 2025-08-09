659966b1a2af8d4fe9fc17e8733715ea01507c43
Mechular <18108148+Mechular@users.noreply.github.com>
2025-08-09 10:59:40 -0300

    Update banner.user.js

--- banner.user.js
+++ banner.user.js
@@ -4956,7 +4956,7 @@ function moveCallBtn() {
     const powerDialerModal = document.querySelector('.power-dialer-modal');
     const activeCall = document.querySelector('.contact-details.flex.items-center.justify-center.gap-2');
     
-    if (!isOnContactPage(location.href)) {
+    if (!location.href.include("contacts/detail/")) {
         if (callSellerBtn) callSellerBtn.remove();
         if (nextSeller) nextSeller.remove();
         if (powerDialer) powerDialer.remove();
